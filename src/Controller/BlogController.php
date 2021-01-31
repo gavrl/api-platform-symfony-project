a1b92148bfa3a15d1844abda66d3505ef8a83462
SergeyGavr <sergeygavr.94@yandex.ru>
2021-01-31 13:45:43 +0300

    32. Persisting entities and serializing data

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -34,33 +34,34 @@ class BlogController extends AbstractController
     ];
 
     /**
-     * @Route("/", name="blog_list", defaults={"page": 5})
+     * @Route("/{page}", name="blog_list", defaults={"page": 5}, requirements={"page"="\d+"}, methods={"GET"})
      * @param Request $request
      * @param int $page
      * @return Response
      */
     public function list(Request $request, $page = 1): Response
     {
-        $em = $this->getDoctrine()->getManager();
-        $em->getConnection()->connect();
-        $connected = $em->getConnection()->isConnected();
-
-        dump($connected);
         $limit = $request->get('limit', 10);
 
         return $this->json(
             [
                 'page' => $page,
                 'limit' => $limit,
-                'data' => array_map(function ($item) {
-                    return $this->generateUrl('blog_by_id', ['id' => $item['id']]);
-                },self::POSTS)
+                'data' => array_map(
+                    function ($item) {
+                        return $this->generateUrl('blog_by_id', ['id' => $item['id']]);
+                    },
+                    self::POSTS
+                )
             ]
         );
     }
 
     /**
-     * @Route("/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     *
+     * @param $id
+     * @return Response
      */
     public function post($id): Response
     {
@@ -70,12 +71,33 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/{slug}", name="blog_by_slug")
+     * @Route("/post/{slug}", name="blog_by_slug")
+     *
+     * @param $slug
+     * @return Response
      */
     public function postBySlug($slug): Response
     {
         return $this->json(
             self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
         );
+    }
+
+    /**
+     * @Route("/add", name="blog_add", methods={"POST"})
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function add(Request $request): Response
+    {
+        $serializer = $this->get('serializer');
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($blogPost);
+        $em->flush();
+
+        return $this->json($blogPost);
     }
 }
